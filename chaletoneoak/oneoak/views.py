10c229922d31934d2937af0acd1e08df2ea9aef3
akask02109 <dreamtekakash@gmail.com>
2017-02-01 13:57:09 +0530

    updated

--- chaletoneoak/oneoak/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.views.generic.base import TemplateView
-
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-class GalleryView(TemplateView):
-    template_name = 'gallery.html'
